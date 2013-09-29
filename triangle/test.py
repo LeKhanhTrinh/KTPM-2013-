0d0064addeee5c14b0ab8a23fa739fca11a4d454
khanhtrinh <trinhlk_55@vnu.edu.vn>
2013-09-29 23:02:45 +0700

    Hoàn chỉnh
    Thêm 0
    Sửa code để tăng giới hạn dưới

--- triangle/test.py
+++ triangle/test.py
@@ -22,13 +22,13 @@ class test(unittest.TestCase):
         self.assertEqual(detect_triangle(2,0, "-1"), "Gia tri dau vao khong phu hop\n")
         print "Test case 4: a = %r, b = %r, c = %r" % (1,2,"2^32")
         self.assertEqual(detect_triangle(1, 2.0, 2**32), "Gia tri dau vao khong phu hop\n")
-        print "Test case 5: a = %r, b = %r, c = %r" % (0,2,"2^32-1")
-        self.assertEqual(detect_triangle(0, 2.0, 2**32-1), "Gia tri dau vao khong phu hop\n")
         print "Test case 6: a = %r, b = %r, c = %r" % ("2^32-1","2**32","0")
         self.assertEqual(detect_triangle(2**32-1, 2**32, 0), "Gia tri dau vao khong phu hop\n")
         print "Test case 7: a = %r, b = %r, c = %r" % ("1e-10","2**32","2**32-2")
         self.assertEqual(detect_triangle(1e-10 , 2**32, 2**32-2), "Gia tri dau vao khong phu hop\n")
     def testInputValid(self):   # Test invalid.
+        print "Test case 5: a = %r, b = %r, c = %r" % (0,2,"2^32-1")
+        self.assertEqual(detect_triangle(0, 2.0, 2**32-1), "Khong phai tam giac\n")
         print "Test case 8: a = %r, b = %r, c = %r" % (1,4,5)
         self.assertEqual(detect_triangle(1.0, 4.0, 5.0), "Khong phai tam giac\n")
         print "Test case 9: a = %r, b = %r, c = %r" % (1,4,"2**32-1")
@@ -46,7 +46,7 @@ class test(unittest.TestCase):
         self.assertEqual(detect_triangle(5*1e-10, 3*1e-10,4*1e-10), "Tam giac vuong\n")
         print "Test case 15: a = %r, b = %r, c = %r" % (4*2**31, 5*2**31,3*2**31)
         self.assertEqual(detect_triangle(4*2**29, 5*2**29,3*2**29), "Tam giac vuong\n")
-        print "Test case 15: a = %r, b = %r, c = %r" % (8.0,math.sqrt(48),4.0)
+        print "Test case 33: a = %r, b = %r, c = %r" % (8.0,math.sqrt(48),4.0)
         self.assertEqual(detect_triangle(8.0,math.sqrt(48),4.0), "Tam giac vuong\n")
     def testTGcan(self):    # Tam giac can
         print "Test case 16: a = %r, b = %r, c = %r" % (2,2,1)
@@ -62,8 +62,8 @@ class test(unittest.TestCase):
         print "Test case 21: a = %r, b = %r, c = %r" % ("2**32-1","2**32-1",4)
         self.assertEqual(detect_triangle(2**32-1, 2**32-1, 4), "Tam giac can\n")
     # Gioi han 2**26 khi co mot bien < 1
-        print "Test case 22: a = %r, b = %r, c = %r" % ("2**32-1","2**32-1",4)
-        self.assertEqual(detect_triangle(2**26, 2**26, 1**-10), "Tam giac can\n")
+        print "Test case 22: a = %r, b = %r, c = %r" % ("2**32-1","2**32-1","1e-6")
+        self.assertEqual(detect_triangle(2**32-1, 2**32-1, 1e-9), "Tam giac can\n")
     def testTGdeu(self):    # Tam giac deu
         print "Test case 23: a = %r, b = %r, c = %r" % (2,2,2)
         self.assertEqual(detect_triangle(2.0, 2.0, 2.0), "Tam giac deu\n")
@@ -75,7 +75,7 @@ class test(unittest.TestCase):
         print "Test case 26: a = %r, b = %r, c = %r" % (1,1,math.sqrt(2))
         self.assertEqual(detect_triangle(1.0, 1.0, math.sqrt(2.0)), "Tam giac vuong can\n")
         print "Test case 27: a = %r, b = %r, c = %r" % (2,2*math.sqrt(2),2)
-        self.assertEqual(detect_triangle(2.0, 2*math.sqrt(2.0), 2), "Tam giac vuong can\n")
+        self.assertEqual(detect_triangle(2, 2*math.sqrt(2), 2), "Tam giac vuong can\n")
         print "Test case 28: a = %r, b = %r, c = %r" % (2*math.sqrt(2),2,2)
         self.assertEqual(detect_triangle(2*math.sqrt(2.0), 2.0, 2), "Tam giac vuong can\n")
     def testTGthuong(self):     # Tam giac thuong
@@ -84,8 +84,10 @@ class test(unittest.TestCase):
         print "Test case 30: a = %r, b = %r, c = %r" % (2, "2^32-1", "2^32-2")
         self.assertEqual(detect_triangle(2, 2**31-5, 2**31-4), "Tam giac binh thuong\n")
     # Gioi han 10**-5 cho e = 10**-10
-        print "Test case 31: a = %r, b = %r, c = %r" % (2e-5, 3e-5, 4e-5)
-        self.assertEqual(detect_triangle(2e-5, 3e-5, 4e-5), "Tam giac binh thuong\n")
+        print "Test case 31: a = %r, b = %r, c = %r" % (2e-10, 3-5e-10, 3-4e-10)
+        self.assertEqual(detect_triangle(2e-10, 3-5e-10, 3-4e-10), "Tam giac binh thuong\n")
+        print "Test case 32: a = %r, b = %r, c = %r" % (2e-7, 3e-7, 4e-7)
+        self.assertEqual(detect_triangle(2e-7, 3e-7, 4e-7), "Tam giac binh thuong\n")
 def main():
     unittest.main()
 
--- triangle/triangle.py
+++ triangle/triangle.py
@@ -4,23 +4,32 @@ Created on Sep 29, 2013
 @author: KHANHTRINH
 '''
 import math
+from decimal import Decimal, ExtendedContext, setcontext
+ExtendedContext.prec = 60
+setcontext(ExtendedContext)
 #from decimal import getcontext
 def detect_triangle(a, b, c):
 #    getcontext().prec = 10
     #if False not in [type(i) in [float, int] for i in (x,y,z)]
     #if type[a,b,c] in [float, int]:
     if ((type(a) in [float, long, int]) and (type(b) in [float, long, int]) and (type(c) in [float, long, int])
-        and ((a > 0) and (a <= (2**32-1))) and ((b > 0) and (b <= (2**32-1))) and ((c > 0) and (c <= (2**32-1)))):
-        if ((a + b) > c) and ((a + c) > b) and ((c + b) > a):
+        and ((a >= 0) and (a <= (2**32-1))) and ((b >= 0) and (b <= (2**32-1))) and ((c >= 0) and (c <= (2**32-1)))):
+        if (((Decimal(a) + Decimal(b)) > Decimal(c)) 
+            and ((Decimal(a) + Decimal(c)) > Decimal(b)) 
+                and ((Decimal(c) + Decimal(b)) > Decimal(a))):
             if a == b and b == c and c == a:
                 return"Tam giac deu\n"
             elif (a == b) or (b == c) or (c == a):
                 e = 1e-10
-                if (math.fabs(c*c - b*b - a*a) < e) or (math.fabs(b*b - c*c - a*a) < e) or (math.fabs(a*a - b*b - c*c) < e):
+                if ((math.fabs(Decimal(c)**2 - Decimal(b)**2 - Decimal(a)**2) < e and a==b) 
+                    or (math.fabs(Decimal(b)**2 - Decimal(c)**2 - Decimal(a)**2) < e and a==c) 
+                        or (math.fabs(Decimal(a)**2 - Decimal(b)**2 - Decimal(c)**2) < e and b==c)):
                     return "Tam giac vuong can\n"
                 else:
                     return"Tam giac can\n"
-            elif (math.fabs(c*c - b*b - a*a) < 1e-10) or (math.fabs(b*b - c*c - a*a) < 1e-10) or (math.fabs(a*a - b*b - c*c) < 1e-10):
+            elif ((math.fabs(Decimal(c)**2 - Decimal(b)**2 - Decimal(a)**2) < 1e-14) 
+                or (math.fabs(Decimal(b)**2 - Decimal(c)**2 - Decimal(a)**2) < 1e-14) 
+                    or (math.fabs(Decimal(a)**2 - Decimal(b)**2 - Decimal(c)**2) < 1e-14)):
                 return"Tam giac vuong\n"
             else:
                 return"Tam giac binh thuong\n" 
